bc9689e40fa3a89d3a0795952f3e4df42b714738
Lavanya Agrawl <luckygargroxx2510@gmail.com>
2021-07-30 01:54:05 +0530

    4th commit

--- callAssistant.py
+++ callAssistant.py
@@ -1,18 +1,29 @@
 import mainAssistant
 import threading
+import tkinter.font as font
 from tkinter import *
+
 class Window(Frame):
+
     def __init__(self, master=None):
+
         super().__init__(master)
         self.master = master
         self.pack()
         master.title("My Assistant")
-        A = Label(master, text="Here to help you!", bg='red')
+
+        buttonFont1 = font.Font(family='Helvetica', size=16, weight='bold')
+
+        A = Label(master, text="Here to help you!",
+                  bg='yellow', font=buttonFont1)
         A.pack(side="top")
-        #photo = PhotoImage(file = r"C:\Users\lucky\Downloads\MY_ASSISTANT-master\MY_ASSISTANT-master\logo.png")
-        a = Button(master, text="Edith!", width=20, border="20", relief="groove",
-               command=threading.Thread(target=self.Processo_r).start, bg='blue')
+
+        buttonFont2 = font.Font(family='Helvetica', size=20, weight='bold')
+        a = Button(master, text="Edith!", width=20, border="20", relief="groove", command=threading.Thread(target=self.Processo_r).start, bg='blue', font=buttonFont2)
         a.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        b = Button(master, text="Stop", width=10, border="50", relief="groove", command=root.destroy, bg='red', font=buttonFont2)
+        b.place(relx=0.5, rely=0.8, anchor=CENTER)
 
     def Processo_r(self):
         mainAssistant.calling()
@@ -21,4 +32,5 @@ root = Tk()
 
 app = Window(root)
 root.geometry("500x500")
+root.configure(bg='black')
 root.mainloop()
